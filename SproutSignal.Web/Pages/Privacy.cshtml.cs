d25b5112c9b0493b23377cf8d88d78d91d2b3064
Ana S <anacseq@gmail.com>
2026-08-06 13:59:32 +0100

    Set up ASP.NET Core Razor Pages project

---
+++ SproutSignal.Web/Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SproutSignal.Web.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
